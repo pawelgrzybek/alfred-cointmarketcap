aeb8f7ed493fe498b74b5dc3c1b95467eadd50df
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2021-05-11 10:34:36 +0100

    Workflow ready

--- index.js
+++ index.js
@@ -1,120 +1,70 @@
-// "use strict";
-// const fetch = require("node-fetch");
-
-// fetch(`https://api.npms.io/v2/search?q=${process.argv[2]}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     const results = {
-//       items: []
-//     };
-
-//     data.results.map(item => {
-// results.items.push({
-//   title: `${item.package.name}`,
-//   subtitle: `${item.package.description} (version ${
-//     item.package.version
-//   })`,
-//   arg: `https://www.npmjs.com/package/${item.package.name}`,
-//   mods: {
-//     cmd: {
-//       valid: item.package.links.repository ? true : false,
-//       arg: item.package.links.repository,
-//       subtitle: item.package.links.repository
-//         ? "Open repository on Github"
-//         : "Missing repository link"
-//     },
-//     alt: {
-//       valid: item.package.links.bugs ? true : false,
-//       arg: item.package.links.bugs,
-//       subtitle: item.package.links.bugs
-//         ? "Open Issues"
-//         : "Missing bug links"
-//     }
-//   },
-//   text: {
-//     copy: `https://www.npmjs.com/package/${item.package.name}`,
-//     largetype: `https://www.npmjs.com/package/${item.package.name}`
-//   }
-// });
-//     });
-//     // eslint-disable-next-line no-console
-//     console.log(JSON.stringify(results));
-//   })
-//   .catch(error => {
-//     // eslint-disable-next-line no-console
-//     console.log(
-//       JSON.stringify({
-//         items: [
-//           {
-//             title: error.name,
-//             subtitle: error.message,
-//             valid: false,
-//             text: {
-//               copy: error.stack
-//             }
-//           }
-//         ]
-//       })
-//     );
-//   });
-
 const fetch = require("node-fetch");
+
+const MISSING_ARGUMENT_MESSAGE = Object.freeze({
+  title: `Incorrect argument`,
+  subtitle: `Example: cpc 0.02 BTC GBP`,
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  title: `Incorrect request`,
+  subtitle: `Are you sure thi coin exists? How about your API key?`,
+});
 
 async function main() {
   const results = {
     items: [],
   };
 
-  const [
-    value,
-    coinSource = "",
-    coinTarget = "gbp",
-  ] = process.argv[2].trim().split(" ");
+  const [apiKey, coinOutputDefault, value, coinSource = "", coinOutput] =
+    process.argv[2].trim().split(" ");
 
   const coinSourceUpperCase = coinSource.toUpperCase();
-  const coinTargetUpperCase = coinTarget.toUpperCase();
+  const coinTargetUpperCase = (
+    coinOutput ? coinOutput : coinOutputDefault
+  ).toUpperCase();
 
   if (!Number(value) || coinSourceUpperCase === "") {
-    results.items.push({
-      title: `Incorect request`,
-      subtitle: `Example: cpc 0.02 BTC GBP`,
-    });
-
+    results.items.push(MISSING_ARGUMENT_MESSAGE);
     console.log(JSON.stringify(results));
-
     return;
   }
 
   try {
     const response = await fetch(
-      `https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount=${value}&symbol=${coinSource}&convert=${coinTarget}`,
+      `https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount=${value}&symbol=${coinSourceUpperCase}&convert=${coinTargetUpperCase}`,
       {
         headers: {
-          "X-CMC_PRO_API_KEY": "d95bbadf-1e32-4df6-8078-1b42d1d0fc68",
+          "X-CMC_PRO_API_KEY": apiKey,
         },
       }
     );
-    const { data } = await response.json();
-    const result = data.quote[coinTargetUpperCase].price;
+    const {
+      data: { quote, name },
+    } = await response.json();
+    const result = quote[coinTargetUpperCase].price;
+    const coinSourceName = name;
+    const coinSourceNameUrl = coinSourceName.toLowerCase().replace(/ /g, "-");
 
     results.items.push({
       title: result,
-      subtitle: `${value}${coinSourceUpperCase} = ${result}${coinTargetUpperCase}`,
+      subtitle: `${value} ${coinSourceUpperCase} = ${result} ${coinTargetUpperCase}`,
       arg: result,
       text: {
         copy: result,
-        largetype: `${value}${coinSourceUpperCase} = ${result}${coinTargetUpperCase}`,
+        largetype: `${value} ${coinSourceUpperCase} = ${result} ${coinTargetUpperCase}`,
+      },
+      mods: {
+        cmd: {
+          arg: `https://coinmarketcap.com/currencies/${coinSourceNameUrl}/`,
+          subtitle: `Open ${coinSourceName} on CoinMarketCap`,
+        },
       },
     });
   } catch (error) {
-    console.error(error);
-    results.items.push({
-      title: `Incorect request`,
-      subtitle: `Example: cpc 0.02 BTC GBP`,
-    });
+    results.items.push(ERROR_MESSAGE);
+  } finally {
+    console.log(JSON.stringify(results));
   }
-
-  console.log(JSON.stringify(results));
 }
 
 main();
